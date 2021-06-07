0c01fce7f3631e7d41f2e62cc40fefa13029d40f
bouboumael <bouboumael@gmail.com>
2021-06-07 16:51:13 +0200

    save

--- src/Service/GeoService.php
+++ src/Service/GeoService.php
@@ -4,8 +4,10 @@ namespace App\Service;
 
 class GeoService
 {
+    private const HEARTH = 6371;
+
     public function calcDistance(float $lat1, float $lng1, float $lat2, float $lng2): float
     {
-        return ACOS(SIN(deg2rad($lat1)) * SIN(deg2rad($lat2)) + COS(deg2rad($lat1)) * COS(deg2rad($lat2))*COS(deg2rad($lng1 - $lng2))) * 6371;
+        return ACOS(SIN(deg2rad($lat1)) * SIN(deg2rad($lat2)) + COS(deg2rad($lat1)) * COS(deg2rad($lat2)) * COS(deg2rad($lng1 - $lng2))) * self::HEARTH;
     }
 }
